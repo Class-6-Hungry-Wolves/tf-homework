a73769d1002d6ab5a25008bd65e32376d886ae7d
Jourdan Callahan <jourdancallahan8@gmail.com>
2024-11-04 14:04:41 -0500

    Reinitialize repository with proper .gitignore

---
+++ output.tf
@@ -0,0 +1,28 @@
+output "app1" {
+  value = aws_vpc.app1
+}
+
+output "public-us-east-1a" {
+  value = aws_subnet.public-us-east-1a
+}
+
+output "public-us-east-1b" {
+  value = aws_subnet.public-us-east-1b
+}
+
+output "public-us-east-1c" {
+  value = aws_subnet.public-us-east-1c
+}
+
+output "private-us-east-1a" {
+  value = aws_subnet.private-us-east-1a
+}
+
+output "private-us-east-1b" {
+  value = aws_subnet.private-us-east-1b
+}
+
+output "private-us-east-1c" {
+  value = aws_subnet.private-us-east-1c
+}
+
